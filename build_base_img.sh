7d03f75136eac34883b06064eb31029e5c92facb
Bati Zhao <zhaobati@gmail.com>
2018-03-02 15:12:17 +0800

    增加 alpine

--- build_base_img.sh
+++ build_base_img.sh
@@ -3,22 +3,27 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-UBUNTU_VERSION=latest
+ALPINE_VERSION=latest
 OPENJDK_VERSION=8
+#ORACLE_JDK_VERSION=8
 TOMCAT_VERSION=8
 MAVEN_VERSION=3.5.2-jdk-8
 
 HUB_URL=172.31.21.226/ideal
 
-cd ubuntu
-docker build -t $HUB_URL/ubuntu:${UBUNTU_VERSION} .
-docker push $HUB_URL/ubuntu:${UBUNTU_VERSION}
+cd alpine
+docker build -t $HUB_URL/alpine:${ALPINE_VERSION} .
+docker push $HUB_URL/alpine:${ALPINE_VERSION}
 
-cd ../java/openjdk/8
-docker build -t $HUB_URL/openjdk:${OPENJDK_VERSION} .
-docker push $HUB_URL/openjdk:${OPENJDK_VERSION}
+cd ../java/alpine/openjdk/8
+docker build -t $HUB_URL/alpine-openjdk:${OPENJDK_VERSION} .
+docker push $HUB_URL/alpine-openjdk:${OPENJDK_VERSION}
 
-cd ../../../tomcat
+#cd ../java/alpine/oracle/8
+#docker build -t $HUB_URL/alpine-jdk:${ORACLE_JDK_VERSION} .
+#docker push $HUB_URL/alpine-jdk:${ORACLE_JDK_VERSION}
+
+cd ../../../../tomcat
 docker build -t $HUB_URL/tomcat:${TOMCAT_VERSION} .
 docker push $HUB_URL/tomcat:${TOMCAT_VERSION}
 
