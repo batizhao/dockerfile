6bd85c81233e66aae605d23bbb9242e8fbc006aa
batizhao <zhaobati@gmail.com>
2021-01-13 12:40:22 +0800

    Update.

--- build_base_img.sh
+++ build_base_img.sh
@@ -4,9 +4,9 @@ set -o nounset
 set -o pipefail
 
 ALPINE_VERSION=latest
-OPENJDK_VERSION=8
-#ORACLE_JDK_VERSION=8
-TOMCAT_VERSION=9
+OPENJDK_VERSION=11
+ORACLE_JDK_VERSION=8
+TOMCAT_VERSION=9-jdk8
 MAVEN_VERSION=latest
 
 HUB_URL=harbor.pecado.com/ideal
@@ -16,18 +16,18 @@ cd alpine
 docker build -t $HUB_URL/alpine:${ALPINE_VERSION} .
 docker push $HUB_URL/alpine:${ALPINE_VERSION}
 
-cd ../java/alpine/openjdk/8
+cd ../java/alpine/openjdk/${OPENJDK_VERSION}
 docker build -t $HUB_URL/alpine-openjdk:${OPENJDK_VERSION} .
 docker push $HUB_URL/alpine-openjdk:${OPENJDK_VERSION}
 
-#cd ../java/alpine/oracle/8
-#docker build -t $HUB_URL/alpine-jdk:${ORACLE_JDK_VERSION} .
-#docker push $HUB_URL/alpine-jdk:${ORACLE_JDK_VERSION}
+# cd ../java/alpine/oracle/${ORACLE_JDK_VERSION}
+# docker build -t $HUB_URL/alpine-jdk:${ORACLE_JDK_VERSION} .
+# docker push $HUB_URL/alpine-jdk:${ORACLE_JDK_VERSION}
 
-cd ../../../../tomcat
-docker build -t $HUB_URL/tomcat:${TOMCAT_VERSION} .
-docker push $HUB_URL/tomcat:${TOMCAT_VERSION}
+# cd ../../../../tomcat
+# docker build -t $HUB_URL/tomcat:${TOMCAT_VERSION} .
+# docker push $HUB_URL/tomcat:${TOMCAT_VERSION}
 
-cd ../maven
-docker build -t $HUB_URL/maven:${MAVEN_VERSION} .
-docker push $HUB_URL/maven:${MAVEN_VERSION}
+# cd ../maven
+# docker build -t $HUB_URL/maven:${MAVEN_VERSION} .
+# docker push $HUB_URL/maven:${MAVEN_VERSION}
