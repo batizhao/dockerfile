e305d727daacb1fe04b2b7d54f0fb1b1983c06ec
batizhao <zhaobati@gmail.com>
2023-05-23 11:20:09 +0800

    切换到 centos 镜像，解决 alpine jvm crash 的问题

--- build_base_img.sh
+++ build_base_img.sh
@@ -9,7 +9,7 @@ ORACLE_JDK_VERSION=8
 TOMCAT_VERSION=9-jdk8
 MAVEN_VERSION=latest
 
-HUB_URL=harbor.pecado.com:8888/ideal
+HUB_URL=172.31.10.85:5000/library
 # HUB_URL=batizhao
 
 cd alpine
@@ -20,9 +20,9 @@ cd ../java/alpine/openjdk
 docker build -t $HUB_URL/alpine-openjdk:${OPENJDK_VERSION} .
 docker push $HUB_URL/alpine-openjdk:${OPENJDK_VERSION}
 
-# cd ../java/alpine/oracle/${ORACLE_JDK_VERSION}
-# docker build -t $HUB_URL/alpine-jdk:${ORACLE_JDK_VERSION} .
-# docker push $HUB_URL/alpine-jdk:${ORACLE_JDK_VERSION}
+# cd ../java/centos/oracle/${ORACLE_JDK_VERSION}
+# docker build -t $HUB_URL/centos7-jdk:${ORACLE_JDK_VERSION} .
+# docker push $HUB_URL/centos7-jdk:${ORACLE_JDK_VERSION}
 
 # cd ../../../../tomcat
 # docker build -t $HUB_URL/tomcat:${TOMCAT_VERSION} .
