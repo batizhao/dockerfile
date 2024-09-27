b282cd218d3c29669c1404ba259ba3d1623c008d
batizhao <zhaobati@gmail.com>
2024-09-27 10:09:40 +0800

    代码：更新 harbor 仓库。

--- build_base_img.sh
+++ build_base_img.sh
@@ -14,24 +14,20 @@ HUB_URL=172.31.25.221:5000/library
 # HUB_URL=batizhao
 
 cd alpine
-docker build -t $HUB_URL/alpine:${ALPINE_VERSION} .
+docker build --platform=linux/amd64 -t $HUB_URL/alpine:${ALPINE_VERSION} .
 docker push $HUB_URL/alpine:${ALPINE_VERSION}
 
+cd ../ubuntu
+docker build --platform=linux/amd64 -t $HUB_URL/ubuntu .
+docker push $HUB_URL/ubuntu
+
 cd ../java/alpine
-docker build -t $HUB_URL/openjdk${OPENJDK_VERSION}-alpine .
+docker build --platform=linux/amd64 -t $HUB_URL/openjdk${OPENJDK_VERSION}-alpine .
 docker push $HUB_URL/openjdk${OPENJDK_VERSION}-alpine
 
-# cd ../java/centos/oracle/${ORACLE_JDK_VERSION}
-# docker build -t $HUB_URL/centos7-jdk:${ORACLE_JDK_VERSION} .
-# docker push $HUB_URL/centos7-jdk:${ORACLE_JDK_VERSION}
-
-# cd ../../../../tomcat
-# docker build -t $HUB_URL/tomcat:${TOMCAT_VERSION} .
-# docker push $HUB_URL/tomcat:${TOMCAT_VERSION}
-
-# cd ../maven
-# docker build -t $HUB_URL/maven:${MAVEN_VERSION} .
-# docker push $HUB_URL/maven:${MAVEN_VERSION}
+cd ../ubuntu/openjdk
+docker build --platform=linux/amd64 -t $HUB_URL/openjdk${OPENJDK_VERSION}-ubuntu .
+docker push $HUB_URL/openjdk${OPENJDK_VERSION}-ubuntu
 
 # docker pull $SRC_URL/stalber:master_20240904032654_b95b8f0b_13
 # docker tag $SRC_URL/stalber:master_20240904032654_b95b8f0b_13 $HUB_URL/stalber:master_20240904032654_b95b8f0b_13
